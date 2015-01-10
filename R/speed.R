89569cdc7cc899d5867a463e13405cb98b12e41f
Simon Garnier <simon.garnier@gmail.com>
2015-01-10 14:42:34 -0500

    Added documentation for linear speed and acceleration functions

--- R/speed.R
+++ R/speed.R
@@ -3,6 +3,31 @@
 #' Given a series of spatio-temporal 2D coordinates, this function returns the 
 #' instantaneous linear speed as the distance between successive positions 
 #' divided by the time between these successive positions. 
+#' 
+#' @param x Vector of x coordinates 
+#' @param y Vector of y coordinates
+#' @param t Vector of time stamps
+#' 
+#' @return This function returns a vector with the same length as x, y and t
+#' corresponding to the instantaneous linear speed along the [x, y, t].
+#' trajectory.
+#' 
+#' @author Simon Garnier: \email{garnier@@njit.edu}, \link[https://twitter.com/sjmgarnier]{@@sjmgarnier}
+#' 
+#' @seealso \code{\link{linear_acceleration}}
+#' 
+#' @examples
+#' # Generate and plot a random smooth trajectory
+#' x <- filter(cumsum(rnorm(1001)), rep(1 / 10, 10))
+#' y <- filter(cumsum(rnorm(1001)), rep(1 / 10, 10))
+#' t <- 0:1000
+#' plot(y ~ x, type = "l", asp = 1)
+#' 
+#' # Compute and plot linear speed
+#' s <- linear_speed(x, y, t)
+#' plot(s ~ t, type = "l")
+#' 
+#' @export
 #'
 linear_speed <- function(x, y, t) {
   dt <- diff(t)
@@ -10,12 +35,48 @@ linear_speed <- function(x, y, t) {
   c(NA, dp / dt)
 }
 
+
+
 #' Compute instantaneous linear acceleration
 #'
 #' Given a series of instantaneous linear speeds as computed by 
-#' \link[swarmR]{linear_speed}, this function returns the instantaneous linear 
+#' \code{\link{linear_speed}}, this function returns the instantaneous linear 
 #' acceleration as the difference between successive instantaneous linear speeds. 
+#'
+#' @param s Vector of speeds
+#' 
+#' @return This function returns a vector with the same length as s 
+#' corresponding to the instantaneous linear acceleration.
+#' 
+#' @author Simon Garnier: \email{garnier@@njit.edu}, \link[https://twitter.com/sjmgarnier]{@@sjmgarnier}
+#' 
+#' @seealso \code{\link{linear_speed}}
+#' 
+#' @examples
+#' # Generate and plot a random smooth trajectory
+#' x <- filter(cumsum(rnorm(1001)), rep(1 / 10, 10))
+#' y <- filter(cumsum(rnorm(1001)), rep(1 / 10, 10))
+#' t <- 0:1000
+#' plot(y ~ x, type = "l", asp = 1)
+#' 
+#' # Compute and plot linear speed
+#' s <- linear_speed(x, y, t)
+#' plot(s ~ t, type = "l")
+#' 
+#' # Compute and plot linear acceleration
+#' a <- linear_acceleration(s)
+#' plot(s ~ t)
+#' 
+#' @export
 #'
 linear_acceleration <- function(s) {
   c(NA, diff(s))
 }
+
+
+
+
+
+
+
+
