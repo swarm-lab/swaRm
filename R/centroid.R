66da5379567fe486d3d5534fa727b5cf27ebcba6
Simon Garnier <garnier@njit.edu>
2021-12-23 10:19:32 +0100

    Add robust group centroid calculation.

--- R/centroid.R
+++ R/centroid.R
@@ -7,6 +7,11 @@
 #' 
 #' @param y A vector of y (or latitude) coordinates.
 #' 
+#' @param robust A logical value indicating whether to compute the centroid as a
+#'  simple average of the coordinates (FALSE, the default), or as the average of 
+#'  the coordinates weighted by the inversd of their mean pairwise distance to 
+#'  all other coordinates in the set (TRUE).  
+#' 
 #' @param geo A logical value indicating whether the locations are defined by 
 #'  geographic coordinates (pairs of longitude/latitude values). Default: FALSE.
 #' 
@@ -20,23 +25,38 @@
 #' # TODO
 #' 
 #' @export
-centroid <- function(x, y, geo = FALSE) {
+centroid <- function(x, y, robust = FALSE, geo = FALSE) {
   if (length(x) != length(y)) 
     stop("x and y should have the same length.")
   
   if (!is.numeric(x) | !is.numeric(y))
     stop("x and y should be numeric.")
   
+  if (robust) {
+    pd <- pdist(x, y, geo = geo)
+    diag(pd) <- NA
+    w <- 1 / colMeans(pd, na.rm = TRUE)
+  }
+
   if (geo) {
     if (length(x) > 1) {
-      centrd <- tibble::as_tibble(geosphere::geomean(cbind(x, y)))
+      if (robust) {
+        centrd <- tibble::as_tibble(geosphere::geomean(cbind(x, y), w))
+      } else {
+        centrd <- tibble::as_tibble(geosphere::geomean(cbind(x, y)))
+      }
       names(centrd) <- c("lon", "lat")
     } else {
       centrd <- tibble::tibble(lon = x, lat = y)
     }
   } else {
-    centrd <- tibble::tibble(x = mean(x, na.rm = TRUE), 
-                             y = mean(y, na.rm = TRUE))
+    if (robust) {
+      centrd <- tibble::tibble(x = stats::weighted.mean(x, w, na.rm = TRUE), 
+                               y = stats::weighted.mean(y, w, na.rm = TRUE))
+    } else {
+      centrd <- tibble::tibble(x = mean(x, na.rm = TRUE), 
+                               y = mean(y, na.rm = TRUE))
+    }
   }
   
   centrd
@@ -51,6 +71,11 @@ centroid <- function(x, y, geo = FALSE) {
 #' 
 #' @param y A vector of y (or latitude) coordinates.
 #' 
+#' @param robust A logical value indicating whether to compute the centroid as a
+#'  simple average of the coordinates (FALSE, the default), or as the average of 
+#'  the coordinates weighted by the inversd of their mean pairwise distance to 
+#'  all other coordinates in the set (TRUE).    
+#' 
 #' @param geo A logical value indicating whether the locations are defined by 
 #'  geographic coordinates (pairs of longitude/latitude values). Default: FALSE.
 #' 
@@ -63,8 +88,8 @@ centroid <- function(x, y, geo = FALSE) {
 #' # TODO
 #' 
 #' @export
-dist2centroid <- function(x, y, geo = FALSE) {
-  centrd <- as.matrix(centroid(x, y, geo = geo))
+dist2centroid <- function(x, y, robust = FALSE, geo = FALSE) {
+  centrd <- as.matrix(centroid(x, y, robust = robust, geo = geo))
   
   if (geo) {
     m1 <- matrix(centrd, nrow = length(x), ncol = 2, byrow = TRUE)
@@ -73,21 +98,4 @@ dist2centroid <- function(x, y, geo = FALSE) {
   } else {
     sqrt((x - centrd[1]) ^ 2 + (y - centrd[2]) ^ 2)
   }
-}
-
-
-#' @export
-robustCentroid <- function(x, y){
-  if (length(x) != length(y)) 
-    stop("x and y should have the same length.")
-  
-  if (!is.numeric(x) | !is.numeric(y))
-    stop("x and y should be numeric.")
-  
-  ch <- isChull(x, y)
-  
-  tmp <- rsdepth::centroid(x[ch > 0][order(ch[ch > 0])],
-                           y[ch > 0][order(ch[ch > 0])])
-  
-  tibble::tibble(x = tmp[1], y = tmp[2])
 }
